66a7a51323843f03d16c0b02fdcc052ac3c96e78
Nicole Rauch <info@nicole-rauch.de>
2019-02-09 21:58:44 +0100

    Template for the subradicals.

---
+++ generateSubRadicalTemplate.js
@@ -0,0 +1,21 @@
+var radicals = require("./api_radicals.json");
+var subradicalInfo = require("./subradicalInfo.js").subradicalInfo;
+var fs = require('fs');
+
+const strippedRadicals = radicals.data.map(radical => ({
+    level: radical.data.level,
+    slug: radical.data.slug,
+}));
+
+let orderedRadicals = strippedRadicals.sort((r1, r2) => (r1.level - r2.level) === 0 ?
+    (r1.slug < r2.slug ? -1 : 1) : (r1.level - r2.level));
+
+
+let radicalsString = "const x = {";
+
+orderedRadicals.forEach(radical =>
+    radicalsString += '"' + radical.slug + '": [],'
+)
+radicalsString += "};";
+
+fs.writeFileSync('subradicalInfoTemplate.js', radicalsString, 'utf8');
