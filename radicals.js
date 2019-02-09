5e6e373a1e24cf40ec88d611735ae1c13c59c69c
Nicole Rauch <info@nicole-rauch.de>
2019-02-09 14:05:44 +0100

    The first ten levels of radicals

---
+++ radicals.js
@@ -0,0 +1,31 @@
+var radicals = {
+  // Level 1 - 10
+  barb: [],
+  big: [],
+  construction: [],
+  cross: [],
+  drop: [],
+  enter: [],
+  fins: [],
+  ground: [],
+  gun: [],
+  leaf: [],
+  lid: [],
+  mountain: [],
+  mouth: [],
+  nine: [],
+  person: [],
+  power: [],
+  prison: [],
+  river: [],
+  seven: [],
+  slide: [],
+  stick: [],
+  sun: [],
+  toe: [],
+  tree: [],
+  two: [],
+  woman: [],
+  // Level 11 - 20
+  
+};
