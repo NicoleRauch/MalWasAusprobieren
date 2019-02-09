2f8a1992be2e777d4a7c719e86447499369af3f7
Nicole Rauch <info@nicole-rauch.de>
2019-02-09 21:20:23 +0100

    Now we extract data from the V2 API and use that

---
+++ stripRadicalsData.js
@@ -0,0 +1,16 @@
+var radicals = require("./api_radicals.json");
+var fs = require('fs');
+
+const strippedRadicals = radicals.data.map(radical => ({
+    id: radical.id,
+    level: radical.data.level,
+    slug: radical.data.slug,
+    characters: radical.data.characters,
+    character_images: radical.data.characters ? [] : radical.data.character_images,
+
+}));
+
+let orderedRadicals = strippedRadicals.sort((r1, r2) => (r1.level - r2.level) === 0 ?
+    (r1.slug < r2.slug ? -1 : 1) : (r1.level - r2.level));
+
+fs.writeFileSync('radicals.json', JSON.stringify(orderedRadicals), 'utf8');
